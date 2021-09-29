e5c240147752fe7c3d66b27c3f45081633044a24
Nihar Sheth <niharsheth98@gmail.com>
2021-09-29 16:25:04 +0000

    Wrote host_info.sh script and updated ddl.sql variable types.

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -8,17 +8,17 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_info
     cpu_number       INT       NOT NULL,
     cpu_architecture VARCHAR   NOT NULL,
     cpu_model        VARCHAR   NOT NULL,
-    cpu_mhz          FLOAT     NOT NULL,
-    L2_cache         SMALLINT  NOT NULL,
-    total_mem        INT       NOT NULL,
-    "timestamp"      TIMESTAMP NOT NULL,
+    cpu_mhz          DOUBLE PRECISION NOT NULL,
+    L2_cache         VARCHAR   NOT NULL,
+    total_mem        VARCHAR   NOT NULL,
+    timestamp_info   TIMESTAMP NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (hostname)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
 (
-    "timestamp"    TIMESTAMP NOT NULL,
+    timestamp_usage TIMESTAMP NOT NULL,
     host_id        SERIAL    NOT NULL,
     memory_free    INT       NOT NULL,
     cpu_idle       INT       NOT NULL,
@@ -26,4 +26,4 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
     disk_io        INT       NOT NULL,
     disk_available INT       NOT NULL,
     CONSTRAINT fk_host_info FOREIGN KEY (host_id) REFERENCES host_info (id)
-);
+    );
