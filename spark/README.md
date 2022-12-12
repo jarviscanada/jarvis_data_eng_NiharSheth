64dd4d647f95e8cab10376812c489509aaac6350
Nihar Sheth <niharsheth98@gmail.com>
2022-12-12 12:57:28 -0500

    Spark project init

---
+++ spark/README.md
@@ -0,0 +1,2 @@
+# Spark/Scala Project
+This project is under development. Since this project follows the GitFlow, the final work will be merged to the master branch after Team Code Team.
---
+++ spark/src/main/scala/ca/jrvs/App.scala
@@ -0,0 +1,11 @@
+package ca.jrvs
+
+object App {
+
+  def foo(x: Array[String]) = x.foldLeft("")((a, b) => a + " " + b)
+
+  def main(args: Array[String]) {
+    println("Hello World!")
+    println("concat arguments = " + foo(args))
+  }
+}
